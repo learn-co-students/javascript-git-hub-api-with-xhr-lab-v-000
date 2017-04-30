43be11d4cd821a5a081fd55456739a047a53a9bb
Bob Habig <rfhab2@gmail.com>
2017-04-30 13:21:48 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,39 +8,7 @@ describe('index', () => {
     })
   })
 
-  describe('callback functions', () => {
-    describe('displayCommits', () => {
-      it('parses and displays json values', () => {
-        var resp = { responseText: commitsData() }
-        displayCommits.call(resp)
-        el = document.getElementById("details")
-        expect(el.innerHTML).toMatch(/Monalisa Octocat/)
-        expect(el.innerHTML).toMatch(/octocat/)
-        expect(el.innerHTML).toMatch(/Fix all the bugs/)
-      })
-    })
-
-    describe('displayBranches', () => {
-      it('parses and displays json values', () => {
-        var resp = { responseText: branchesData() }
-        displayBranches.call(resp)
-        el = document.getElementById("details")
-        expect(el.innerHTML).toMatch(/master/)
-      })
-    })
-    describe('displayRepositories', () => {
-      it('parses and displays json values', () => {
-        var resp = { responseText: reposData() }
-        displayRepositories.call(resp)
-        el = document.getElementById("repositories")
-        expect(el.innerHTML).toMatch(/Hello-World/)
-        expect(el.innerHTML).toMatch(/octocat/)
-        expect(el.innerHTML).toMatch(/https:\/\/github.com\/octocat\/Hello-World/)
-      })
-    })
-  })
-
-  describe('xhr functions', () => {
+    describe('xhr functions', () => {
     let xhr
     let requests
     let el
@@ -91,6 +59,39 @@ describe('index', () => {
 
   })
 })
+
+  describe('callback functions', () => {
+    describe('displayRepositories', () => {
+      it('parses and displays json values', () => {
+        var resp = { responseText: reposData() }
+        displayRepositories.call(resp)
+        el = document.getElementById("repositories")
+        expect(el.innerHTML).toMatch(/Hello-World/)
+        expect(el.innerHTML).toMatch(/octocat/)
+        expect(el.innerHTML).toMatch(/https:\/\/github.com\/octocat\/Hello-World/)
+      })
+    })
+
+    describe('displayCommits', () => {
+      it('parses and displays json values', () => {
+        var resp = { responseText: commitsData() }
+        displayCommits.call(resp)
+        el = document.getElementById("details")
+        expect(el.innerHTML).toMatch(/Monalisa Octocat/)
+        expect(el.innerHTML).toMatch(/octocat/)
+        expect(el.innerHTML).toMatch(/Fix all the bugs/)
+      })
+    })
+
+    describe('displayBranches', () => {
+      it('parses and displays json values', () => {
+        var resp = { responseText: branchesData() }
+        displayBranches.call(resp)
+        el = document.getElementById("details")
+        expect(el.innerHTML).toMatch(/master/)
+      })
+    })
+  })
 
 function commitsData() {
   const commits = [
