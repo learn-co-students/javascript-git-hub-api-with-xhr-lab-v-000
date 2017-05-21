e002fb984c6c3bad3b612748eaad43e7b67b7145
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-05-20 23:50:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getRepositories(form) {
-  let username = form.username.value
+  //var username  = form.username.value
+  let username = document.getElementById("username").value
   let request = new XMLHttpRequest
   request.open("GET", "https://api.github.com/users/" + username + "/repos");
   request.addEventListener('load', showRepositories);
@@ -7,7 +8,24 @@ function getRepositories(form) {
 }
 
 
-function showRepositories(event, data) {
-  console.log("hi")
-  alert('hi')
+function showRepositories(event) {
+  var response = JSON.parse(this.responseText)
+  var repoList = response.map((repo) => `<li><a href='#' onclick='getCommits(this)' data-username="${repo.owner.login}" data-repository='${repo.name}'> ${repo.name}</a></li>`).join("")
+  document.getElementById('repositories').innerHTML = `<ul>${repoList}</ul>`
+
+
+}
+
+function getCommits(li) {
+  let request = new XMLHttpRequest
+  request.open("GET", `https://api.github.com/repos/${li.dataset.username}/${li.dataset.repository}/commits`)
+  request.addEventListener("load", displayCommits)
+  request.send()
+}
+
+function displayCommits(event) {
+  let response = JSON.parse(this.responseText)
+  let commitList = response.map((commit) => `<li>${commit.commit.author.name} - ${commit.author.login} - ${commit.commit.message}</li>`).join("")
+  document.getElementById('details').innerHTML = `<ul>${commitList}</ul>`
+
 }
