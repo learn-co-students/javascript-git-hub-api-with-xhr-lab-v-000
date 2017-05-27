92c9b138cdd290eb1312fbe395c2f4c9d38b790d
kkterai <kkterai@gmail.com>
2017-05-27 15:20:50 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+const rootURL = "https://api.github.com"
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getRepositories() {
+  var username = document.getElementById('username').value;
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://api.github.com/users/`+`${username}`+`/repos`)
+  req.send()
+}
