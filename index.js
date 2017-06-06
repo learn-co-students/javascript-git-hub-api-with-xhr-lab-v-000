07b2377ebb3ceeee8483a0e11ff2da66a6ec49d1
John Pfingst <jpfingst@outlook.com>
2017-06-06 18:05:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  const username = document.getElementById("username").value;
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://api.github.com/users/${username}/repos?page=6`)
+  req.send()
+}
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + '<a href="' + r.html_url + '">' + r.name + '</a>'
+                  + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  const username = document.getElementById("username").value;
+  req.addEventListener("load", displayCommits);
+  req.open("GET", `https://api.github.com/repos/${username}/${name}/commits`)
+  req.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + getCommitAuthor(commit) + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
+
+function getCommitAuthor(commit) {
+  return (commit.author != null) ? commit.author.login : 'unk';
+}
