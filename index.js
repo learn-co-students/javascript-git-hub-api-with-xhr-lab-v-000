9535efe0562a3713451b6ba12784a34de51aaa5c
mikka <mikkanthrope@gmail.com>
2017-07-13 00:28:35 -0700

    Done.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function getCommits(el) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   console.log(commits);
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author + '</strong> - ' + commit.commit.author.name + '<br>' + commit.commit.message + '</li>').join('')}</ul>'`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + '<br>' + commit.commit.message + '</li>').join('')}</ul>'`
   document.getElementById("details").innerHTML = commitsList
 }
 
