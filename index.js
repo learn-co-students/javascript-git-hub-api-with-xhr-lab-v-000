86c2b46f2f92b34d79f236d44e637c1fda78caa8
Howard DeVennish <howarddevennish@gmail.com>
2017-07-20 13:32:54 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function showRepositories(event, data) {
   // this is set to the XMLHttpRequest object that fired the event
   let repos = JSON.parse(this.responseText)
   console.log(repos)
+  debugger
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
@@ -9,7 +10,6 @@ function showRepositories(event, data) {
 function getRepositories() {
   const req = new XMLHttpRequest()
   let username = getUserName()
-  debugger
   req.addEventListener("load", showRepositories)
   req.open("GET", `https://api.github.com/users/${username}/repos`)
   req.send()
