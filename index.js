7873a0c1dbcd8eefade318e8f6fa39566a8e4c3d
Bruce Molina <bruce.molina.81@gmail.com>
2017-09-01 00:50:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function displayRepositories(event, data){
   var repos = JSON.parse(this.responseText);
   console.log(repos)
-// username="' + r.owner.login +'"
-  const repoList = `<ul>${repos.map(r => '<li>'+ 'https://github.com/' + r.owner.login + '/' + r.name  + ' - <a href="#" username="' + r.owner.login + '" repository="' + r.owner.url + '" data-repo="' + r.name + '"  onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+
+  const repoList = `<ul>${repos.map(r => '<li>'+ `https://github.com/${r.owner.login}/${r.name}`  + ' - <a href="#" username="' + r.owner.login + '" repository="' + r.owner.url + '" data-repo="' + r.name + '"  onclick="getCommits(this)">Get Commits here</a></li>').join('')}</ul>`
 
   document.getElementById("repositories").innerHTML = repoList
 }
