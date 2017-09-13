8a75adbb6b38202e165c04441f0a489a51d21ddc
github username <github email address>
2017-09-13 13:14:54 -0400

    Done.

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ function getRepositories(){
 
 
 function displayRepositories(){
-  var repos = JSON.parse(this.responseText)
-  //name and link to url
+  const repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+
 }
 
 function getCommits(el){
